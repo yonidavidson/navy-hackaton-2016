05949e5dd4c6d9eb29328f065b0155ce7d050399
Kobi Barac <kobibarac@gmail.com>
2016-12-01 22:30:34 +0200

    Endpoints update

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,10 @@ var globalStatus = {
     madaz: false,
     reportOut: false,
     firemen: false,
-    recommendation1: "",
+    recommendation1:  "אין לכבות את השריפה עם הידרהטץ מתחים לא נותקו",
     recommendation2: "",
-    recommendation3: ""
+    recommendation3: "",
+    message: ""
 };
 
 global.globalStatus = globalStatus;
@@ -34,15 +35,21 @@ router.get('/getStatus', function(req, res, next) {
 router.get('/reset', function (req, res, next) {
     global.globalStatus = {
         power: false,
-            isolation: false,
-            madaz: false,
-            reportOut: false,
-            firemen: false};
+        isolation: false,
+        madaz: false,
+        reportOut: false,
+        firemen: false,
+        recommendation1: "",
+        recommendation2: "",
+        recommendation3: "",
+        message: ""
+    };
     res.status(200).send(global.globalStatus);
 });
 
 router.get('/power', function (req, res, next) {
     global.globalStatus.power = true;
+    global.globalStatus.message = "Power is out. You may enter the compartment!";
     res.status(200).send(global.globalStatus);
 });
 
@@ -71,15 +78,18 @@ router.get('/recommendation1', function (req, res, next) {
     res.status(200).send(global.globalStatus);
 });
 
-
 router.get('/recommendation2', function (req, res, next) {
     global.globalStatus.recommendation2 =  "מתחים נותקו. ניתן להכניס כבאים עם הידרנט";
     res.status(200).send(global.globalStatus);
 });
 
-
 router.get('/recommendation3', function (req, res, next) {
     global.globalStatus.recommendation3 = "הודעה הועברה לגורמי חוץ";
+    res.status(200).send(global.globalStatus);
+});
+
+router.get('/message', function (req, res, next) {
+    global.globalStatus.message = "Power is out. You may enter the compartment!";
     res.status(200).send(global.globalStatus);
 });
 
