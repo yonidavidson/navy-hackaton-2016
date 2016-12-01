4fd6508d5a444b6562234f65361edf0e2e9a1c9a
Kobi Barac <kobibarac@gmail.com>
2016-12-01 17:44:17 +0200

    New route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var TMClient = require('textmagic-rest-client');
+
+
+var globalStatus = {
+    event: {location: 'Hartom', type: 'Fire' },
+    missingPeople: false
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,12 +14,21 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/helloYossi', function(req, res, next) {
-  var globalStatus = {
-      event: {location: 'Hartom', type: 'Fire' },
-      missingPeople: false
-  };
+router.get('/getStatus', function(req, res, next) {
     res.status(200).send(globalStatus);
     //res.render('index', { title: 'hello Yossilevich' });
 });
+
+router.post('/updateStatus', function (req, res, next) {
+    globalStatus = req.body;
+    res.status(200).send(globalStatus);
+});
+
+router.get('/sendSMS', function (req,res,next) {
+    var c = new TMClient('username', 'C7XDKZOQZo6HvhJwtUw0MBcslfqwtp4');
+    c.Messages.send({text: 'test message', phones:'972544668186'}, function(err, res){
+        console.log('Messages.send()', err, res);
+    });
+});
+
 module.exports = router;
