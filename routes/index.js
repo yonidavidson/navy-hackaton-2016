22cd3a28fd8f0ed7ac3e511b704800ad207c25a8
Kobi Barac <kobibarac@gmail.com>
2016-12-01 21:07:11 +0200

    global

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var TMClient = require('textmagic-rest-client');
 
 
 var globalStatus = {
     event: {location: 'Hartom', type: 'Fire' },
     missingPeople: false
 };
+
+global.globalStatus = globalStatus;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,13 +16,13 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/getStatus', function(req, res, next) {
-    res.status(200).send(globalStatus);
+    res.status(200).send(global.globalStatus);
     //res.render('index', { title: 'hello Yossilevich' });
 });
 
 router.post('/updateStatus', function (req, res, next) {
-    globalStatus = req.body;
-    res.status(200).send(globalStatus);
+    global.globalStatus = req.body;
+    res.status(200).send(global.globalStatus);
 });
 
 router.get('/sendSMS', function (req,res,next) {
