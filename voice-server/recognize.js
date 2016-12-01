37fa472a0c7395b813bb1c4fa11dc3231b8fa76c
yonidavidson <yonidavidson@gmail.com>
2016-12-01 22:29:26 +0200

    fetch from url

--- voice-server/recognize.js
+++ voice-server/recognize.js
@@ -2,6 +2,7 @@
 
 const Speech = require('@google-cloud/speech');
 const execSync = require('child_process').execSync;
+const fetch = require('node-fetch')
 
 // [START speech_streaming_mic_recognize]
 const record = require('node-record-lpcm16');
@@ -24,12 +25,17 @@ function streamingMicRecognize () {
     .on('data', (data) =>{
       const message = data.results
       process.stdout.write(message)
-      if (message !== ""){
-        let command = "./talk.sh " + message
-        execSync(command);
-      }
-      
-    });
+      fetch('http://navyhack2016.azurewebsites.net/getStatus')
+      .then(function(res) {
+        return res.json();
+      }).then(function(json) {
+        if (json !== ""){
+          console.log(json);
+          // let command = "./talk.sh " + json
+          // execSync(command);
+        }
+        })
+    })
 
   // Start recording and send the microphone input to the Speech API
   record.start({ sampleRate: 16000 })
